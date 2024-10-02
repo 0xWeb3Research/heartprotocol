8368bb6b8f26108cc25649bdee8dc4d05f551234
Sidharth Punathil <sidharthpunathil714@gmail.com>
2024-10-02 16:15:05 +0530

    feat: added convert as matchmaker

--- interface/app/components/ProfileView.tsx
+++ interface/app/components/ProfileView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function ProfileView({ profile, onEdit, isActivated, onActivate }) {
+export default function ProfileView({ profile, onEdit, isActivated, onActivate, onMatchmakerActivate, matchmakerActivated }) {
   return (
     <div className="p-6 space-y-6">
       {profile.image && (
@@ -29,19 +29,13 @@ export default function ProfileView({ profile, onEdit, isActivated, onActivate }
           <p className="text-sm text-gray-700"><strong className="font-semibold">Relationship Type:</strong> {profile.relationship_type}</p>
         </div>
       </div>
-      <div className="flex justify-end mt-4">
-        <button
-          type="button"
-          onClick={onEdit}
-          className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Edit Profile
-        </button>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-4 space-y-2 md:space-y-0 md:space-x-2">
+       
         {isActivated ? (
           <button
             type="button"
             disabled
-            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 cursor-not-allowed ml-2"
+            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 cursor-not-allowed"
           >
             Activated
           </button>
@@ -49,19 +43,42 @@ export default function ProfileView({ profile, onEdit, isActivated, onActivate }
           <button
             type="button"
             onClick={onActivate}
-            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ml-2"
+            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
             Activate Profile
           </button>
         )}
+        {matchmakerActivated ? (
+          <button
+            type="button"
+            disabled
+            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 cursor-not-allowed"
+          >
+            Matchmaker
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={onMatchmakerActivate}
+            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+          >
+            Be A Matchmaker
+          </button>
+        )}
         <button
           type="button"
-          className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#EA728C] ml-2"
+          className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#EA728C]"
         >
-        Earned: {profile?.earned ? parseInt(profile.earned) : 0} $APT
+          Earned: {profile?.earned ? parseInt(profile.earned) : 0} $APT
+        </button>
+        <button
+          type="button"
+          onClick={onEdit}
+          className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Edit Profile
         </button>
       </div>
     </div>
-    
   );
 }
